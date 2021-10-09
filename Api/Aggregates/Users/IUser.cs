b9ffdf703c3fbed215e71e4a6692d1e2831b12f7
Lulus <snys98@outlook.com>
2021-10-09 09:10:43 +0800

    用户增加已有权限属性

--- Api/Aggregates/Users/IUser.cs
+++ Api/Aggregates/Users/IUser.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-
+using Geex.Common.Abstraction;
 using Geex.Common.BlobStorage.Api.Aggregates.BlobObjects;
 using Geex.Common.Identity.Api.Aggregates.Roles;
 using Geex.Common.Identity.Core.Aggregates.Orgs;
-
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 using MongoDB.Entities;
 
 namespace Geex.Common.Identity.Api.Aggregates.Users
@@ -22,5 +23,6 @@ namespace Geex.Common.Identity.Api.Aggregates.Users
         public IQueryable<Role> Roles => DbContext.Queryable<Role>().Where(x => this.RoleNames.Contains(x.Id));
         IQueryable<Org> Orgs => DbContext.Queryable<Org>().Where(x => this.OrgCodes.Contains(x.Code));
         List<string> OrgCodes { get; set; }
+        List<string> Permissions => DbContext.ServiceProvider.GetService<IMediator>().Send(new GetUserPermissionsRequest(this.Id)).Result.ToList();
     }
 }
